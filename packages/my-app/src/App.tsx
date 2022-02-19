6bd8c025c20f74f5cb1b0551935ade1864571d97
KG LEE <acrkdduf@naver.com>
2022-02-19 22:41:19 +0900

    feat: CRA my-app

---
+++ packages/my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import MuiButton from 'material-ui/dist';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <MuiButton variant='text' buttonText='hello' />
+    </div>
+  );
+};
+
+export default App;
