5622897e45f37694600bca0ad3785faaa17761d8
KG LEE <acrkdduf@naver.com>
2022-02-20 23:45:33 +0900

    fix: test import

--- packages/my-app/src/App.tsx
+++ packages/my-app/src/App.tsx
@@ -1,9 +1,11 @@
 import MuiButton from 'material-ui/dist';
+import ExampleComponent from 'material-ui';
 
 const App = () => {
   return (
     <div className='App'>
       <MuiButton variant='text' buttonText='hello' />
+      <ExampleComponent text='hello' />
     </div>
   );
 };
